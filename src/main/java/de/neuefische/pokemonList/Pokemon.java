2655ec787e8208a335cde00d4a4faec38a2129a9
Samir Schabel <90188949+Starkiller1988@users.noreply.github.com>
2022-02-15 09:54:46 +0100

    added images

--- src/main/java/de/neuefische/pokemonList/Pokemon.java
+++ src/main/java/de/neuefische/pokemonList/Pokemon.java
@@ -3,10 +3,13 @@ package de.neuefische.pokemonList;
 public class Pokemon {
     private final String id;
     private final String name;
+    private final String image;
 
-    public Pokemon(String id, String name){
+    public Pokemon(String id, String name, String image){
         this.id = id;
         this.name = name;
+        this.image = image;
+
 
     }
 
@@ -18,5 +21,7 @@ public class Pokemon {
         return name;
     }
 
-
+    public String getImage() {
+        return image;
+    }
 }
--- src/main/java/de/neuefische/pokemonList/PokemonListController.java
+++ src/main/java/de/neuefische/pokemonList/PokemonListController.java
@@ -15,7 +15,7 @@ public class PokemonListController {
 
     public List<Pokemon> getPokemon(){
 
-        List<Pokemon> pokemon = List.of(new Pokemon("001", "Bulbusaur"), new Pokemon("002", "Charmander"), new Pokemon("003", "Squirtle"));
+        List<Pokemon> pokemon = List.of(new Pokemon("001", "Bulbusaur", "https://cdn.myminifactory.com/assets/object-assets/5e9e5f15b5bc2/images/resize-bulbasaur-original.jpg"), new Pokemon("002", "Charmander", "https://i.etsystatic.com/8607787/r/il/2f20e9/1844034204/il_794xN.1844034204_j15u.jpg"), new Pokemon("003", "Squirtle", "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png"));
         return pokemon;
     }
 
